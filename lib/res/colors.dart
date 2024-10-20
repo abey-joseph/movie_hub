b267f11884629bd0053e9a78fb6805a270802d45
Abey Joseph <abeyna567@gmail.com>
2024-10-20 08:12:37 +0800

    app bar added

---
+++ lib/res/colors.dart
@@ -0,0 +1,9 @@
+// app_colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color greenPrimary = Color(0xFF72BF78); // #72BF78
+  static const Color greenLight = Color(0xFFA0D683); // #A0D683
+  static const Color greenLighter = Color(0xFFD3EE98); // #D3EE98
+  static const Color yellowLight = Color(0xFFFEFF9F); // #FEFF9F
+}
