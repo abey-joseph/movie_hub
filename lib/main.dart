57f3f49ae68991b23c33ea8fd911fa696a049eb9
Abey Joseph <abeyna567@gmail.com>
2024-11-01 13:20:29 +0800

    dotenv added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 //import 'package:movie_hub/res/colors.dart';
 import 'package:movie_hub/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MovieHub());
 }
 
